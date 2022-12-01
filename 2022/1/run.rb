9763d722831c92fa01f0bcd7d3594f83e4e61793
Jeremy Baker <jeremy@retailzipline.com>
2022-12-01 15:49:36 -0800

    Day 1 - Improvement based on Paulos code

--- 2022/1/run.rb
+++ 2022/1/run.rb
@@ -1,6 +1,6 @@
-calorie_data = File.read(ARGV[0]).split("\n")
+calorie_data = File.read(ARGV[0]).split("\n\n")
 
-calories_by_elf = calorie_data.join(',').split(',,').collect { |group| group.split(',').sum { |v| v.to_i } }
+calories_by_elf = calorie_data.collect { |group| group.split("\n").sum { |v| v.to_i } }
 top_3_calories = calories_by_elf.max(3).sum
 max_calories = calories_by_elf.max
 elf_number = calories_by_elf.index(max_calories) + 1
